da6f94edbe4afd6a522eeb3d1df9cfc35a67843c
HP <aliimam@hacettepe.edu.tr>
2023-04-18 16:20:47 +0300

    CRUD-DEMO-REST

--- src/main/java/com/luv2code/springboot/cruddemo/dao/abc.java
+++
@@ -1,7 +0,0 @@
-package com.luv2code.springboot.cruddemo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface abc extends JpaRepository<EmployeeDAO, Integer> {
-
-}
