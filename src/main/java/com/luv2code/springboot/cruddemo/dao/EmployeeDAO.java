a33c52a4bad0c7159074589484f6ae9728f13af4
HP <aliimam@hacettepe.edu.tr>
2023-04-18 14:45:46 +0300

    CRUD-DEMO-REST

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -1,11 +1,12 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmployeeDAO {
-    List<Employee> findAll();
+public interface EmployeeDAO  {
+   List<Employee> findAll();
 
     Employee findById(int theId);
 
